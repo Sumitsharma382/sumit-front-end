befbac2f137be6a597c57f74979a460d91b8ab1a
Sumitsharma382 <94230739+Sumitsharma382@users.noreply.github.com>
2023-02-09 01:35:16 +0530

    Add files via upload

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FooterTop from "../footer-top";
+import FooterButtom from "../footer-bottom";
+const Footer=()=>{
+     
+    return (
+        <div className="footer-main">
+      < FooterTop/>
+      < FooterButtom/>
+    </div>
+    )
+
+};
+
+export default Footer;
